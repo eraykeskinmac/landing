752d7d5232d9094eee43ef5690f56ce6cfccf7ba
Sahil Zambani <73684596+sahil-develops@users.noreply.github.com>
2024-05-15 00:10:37 +0530

    chore: Add Vite configuration files, PostCSS configuration, and TypeScript configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Hero from './components/Hero/Hero'
+
+function App() {
+
+
+  return (
+    <div className='flex justify-center items-center mx-auto max-w-7xl flex-col z-40'>
+  <Hero/>
+    </div>
+  )
+}
+
+export default App
