0cc113d4943750b5534c83c8e51f99ebc8a760ab
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-16 15:10:35 +0600

    sonner toast added

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import "@/styles/globals.css";
 import { Navbar } from "@/components/navbar";
 import Footer from "./components/footer";
 import RootProvider from "@/components/root-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RootProvider>
+      <Toaster richColors position="top-center" toastOptions={{}} />
       <header className="bg-[var(--bg-hero)] w-full">
         <Navbar />
       </header>
