389e958810083306a75d4d635faa05c439bcb843
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-16 13:27:58 +0600

    ```text feat: Add Navbar component
    This commit adds the Navbar component to the codebase. The Navbar component is responsible for displaying the navigation bar of the website, including the logo, menu items, and mobile navigation. It enhances the visual appeal and user experience of the website by providing easy navigation and access to different sections.

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { MovingBorderButton } from "@/components/ui/moving-border";
+import { AnimatePresence, motion } from "framer-motion";
+import { Icons } from "@/components/icons";
+import { defaultNavConfig } from "@/config/navbar.config";
+import { cn } from "@/lib/utils";
+import MobileNav from "./mobile-navbar";
+
+export const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const pathName = window.location.pathname;
+
+  return (
+    <div className="flex relative justify-between max-w-7xl mx-auto py-4 items-center lg:px-0 px-6">
+      <a href="/">
+        <Icons.Logo />
+      </a>
+
+      <div className="lg:flex hidden gap-x-5 flex-row justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        {defaultNavConfig.map((item) => {
+          return (
+            <a
+              href={item.href}
+              key={item.href}
+              className={cn(
+                "hover:text-primary duration-75 transition-colors",
+                pathName !== item.href && "text-muted-foreground"
+              )}
+              aria-disabled={item.disable}
+            >
+              {item.title}
+            </a>
+          );
+        })}
+      </div>
+
+      <div className="flex justify-center items-center gap-x-4">
+        <MovingBorderButton
+          borderRadius="2rem"
+        >
+          Join Waitlist
+        </MovingBorderButton>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="lg:hidden block"
+          onClick={() => setShowNavbar(!showNavbar)}
+        >
+          {showNavbar ? (
+            <motion.img
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.2 }}
+              width="24"
+              height="24"
+              src="https://img.icons8.com/material-rounded/24/ffffff/multiply--v1.png"
+              alt="multiply--v1"
+            />
+          ) : (
+            <motion.svg
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.2 }}
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </motion.svg>
+          )}
+        </motion.div>
+      </div>
+      <AnimatePresence>
+        {showNavbar && <MobileNav />}
+      </AnimatePresence>
+    </div>
+  );
+};
