1818f66470fec2c2bdc9d9403f71fb6d076c79a8
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-16 18:59:12 +0600

    feat: Add lazy loading for images in Grid1 component

--- src/components/features/grids/grid-1.tsx
+++ src/components/features/grids/grid-1.tsx
@@ -1,7 +1,61 @@
+import { motion, useInView } from "framer-motion";
+import { useState, useEffect, useRef } from "react";
+
 export default function Grid1() {
   return (
-    <div className="border w-full h-full rounded-2xl flex flex-col justify-center items-center text-center bg-secondary">
-      grid1
+    <div className="border w-full rounded-2xl text-center bg-secondary/40 p-2 md:p-5 flex flex-col justify-center items-center gap-3">
+      <motion.h1
+        className="lg:text-xl"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+      >
+        24/7 Workflow
+      </motion.h1>
+      <motion.p
+        className="text-sm text-muted-foreground"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2 }}
+      >
+        Boost productivity with our agents that work non-stop, available all
+        day.
+      </motion.p>
+      <div className="relative w-full aspect-square bg-secondary/30 border rounded-xl p-3">
+        <motion.div
+          className="border w-full h-full rounded-full radial-gradient flex justify-center items-center"
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+        />
+        <CountDown />
+      </div>
     </div>
-  )
+  );
 }
+
+const CountDown = () => {
+  const ref = useRef(null);
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const isInView = useInView(ref);
+
+  useEffect(() => {
+    if (!isInView) return;
+    const interval = setInterval(() => {
+      if (count === 24 && count2 === 7) {
+        clearInterval(interval);
+      } else {
+        setCount(count + 1);
+        setCount2(count2 < 7 ? count2 + 1 : count2)
+      }
+    }, 50);
+
+    return () => clearInterval(interval);
+  }, [count, count2, isInView]);
+
+  return (
+    <motion.h1 ref={ref} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-2xl md:text-4xl lg:text-6xl primary-gradient cursor-default">
+      {count}/<motion.span>{count2}</motion.span>
+    </motion.h1>
+  );
+};
