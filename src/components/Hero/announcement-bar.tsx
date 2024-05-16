cc1a3dd28e6b70593e62fc7e572db3ece63e67c9
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-16 19:00:07 +0600

    changed style

--- src/components/Hero/announcement-bar.tsx
+++ src/components/Hero/announcement-bar.tsx
@@ -3,12 +3,12 @@ import { ArrowRight } from "lucide-react";
 export default function AnnouncementBar() {
   return (
     <div
-      className="bg-[#171F05] border-[#364C09] border border-b-0 rounded-full flex justify-between p-1 text-xs items-center gap-2 shine shine-hover hover:scale-105 duration-100 transition-transform transform-gpu ease-in-out cursor-pointer group"
+      className="nav-gradient border-[#1c094c] border border-b-0 rounded-full flex justify-between p-1 text-xs items-center gap-2 shine shine-hover hover:scale-105 duration-100 transition-transform transform-gpu ease-in-out cursor-pointer group"
       onClick={() => {
         document.getElementById("waitlist-form")?.focus();
       }}
     >
-      <div className="px-[0.35rem] py-[0.125rem] bg-[#5661F6] rounded-xl">
+      <div className="px-[0.35rem] py-[0.125rem] bg-violet-900 rounded-xl">
         New
       </div>
       <p>Announcing our waitlist</p>
