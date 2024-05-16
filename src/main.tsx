ccd1f16d1d2eb91fc56916b49fa9bab1b20c824b
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-16 13:26:35 +0600

    chore: Update main.tsx to use RootProvider for layout
    The main.tsx file has been updated to use the RootProvider component for layout. This change improves the organization and readability of the codebase by providing a centralized location for managing the header and footer components. Additionally, the import paths have been updated to use the "@" alias for improved clarity.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import Navbar from './components/Navbar/Navbar.tsx'
-import Footer from './components/Footer/Footer.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App.tsx";
+import "@/styles/globals.css";
+import { Navbar } from "@/components/navbar";
+import Footer from "./components/footer";
+import RootProvider from "@/components/root-provider";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Navbar />
-    <App />
-    <Footer />
-  </React.StrictMode>,
-)
+    <RootProvider>
+      <header className="bg-[var(--bg-hero)] w-full">
+        <Navbar />
+      </header>
+      <App />
+      <Footer />
+    </RootProvider>
+  </React.StrictMode>
+);
