428a3bb428b8f03f030f0af1ca825e18d9b301ff
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-16 19:00:00 +0600

    updated

--- src/components/features/grids/grid-5.tsx
+++ src/components/features/grids/grid-5.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+
 export default function Grid5() {
   return (
-    <div className="col-span-2 border w-full h-full rounded-2xl bg-secondary flex flex-col justify-center items-center">
-      grid5
+    <div className="col-span-2 border w-full rounded-2xl bg-secondary flex flex-col justify-between p-2 md:p-5">
+      <h1 className="text-xl md:text-4xl lg:text-5xl font-bold">Higher quality output and Consistency</h1>
+      <p className="text-2xl text-muted-foreground">
+        Consistently deliver top-notch results with our reliable system,
+        ensuring excellence in every task, just like having a dependable
+        teammate by your side.
+      </p>
+      <div>
+        <Button className="rounded-full" size="lg">
+          <a href="/about">
+            Explore more
+          </a>
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
