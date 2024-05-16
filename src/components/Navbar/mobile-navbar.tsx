7229daedf7f9a3749a3c205bbd13de8c3f6bad3b
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-16 13:28:04 +0600

    feat: Add MobileNav component
    This commit adds the MobileNav component to the codebase. The MobileNav component is responsible for displaying the mobile navigation menu of the website. It includes the logo, menu items, and navigation functionality. This addition enhances the user experience by providing easy navigation on mobile devices.

---
+++ src/components/Navbar/mobile-navbar.tsx
@@ -0,0 +1,46 @@
+import { motion } from "framer-motion";
+import { Icons } from "@/components/icons";
+import { Separator } from "@/components/ui/separator";
+import { defaultNavConfig } from "@/config/navbar.config";
+import { cn } from "@/lib/utils";
+
+const variants = {
+  visible: { x: 0, transition: { stiffness: 200 } },
+  hidden: { x: "-100%", transition: { stiffness: 200 } },
+};
+
+export default function MobileNav() {
+  const pathName = window.location.pathname;
+
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      variants={variants}
+      className="absolute  px-5 z-50 flex justify-start items-start gap-y-5 flex-col h-screen rounded-r-xl py-5 left-0 top-5  max-w-[200px] w-full bg-secondary"
+    >
+      <a href="/">
+        <Icons.Logo />
+      </a>
+      <Separator className="bg-background/30 h-1" />
+      <div className="flex bg-transparent gap-x-5 flex-col gap-y-4 justify-start items-start  ">
+        {defaultNavConfig.map((item) => {
+          return (
+            <a
+              href={item.href}
+              key={item.href}
+              className={cn(
+                "hover:text-primary duration-75 transition-colors",
+                pathName !== item.href && "text-muted-foreground"
+              )}
+              aria-disabled={item.disable}
+            >
+              {item.title}
+            </a>
+          );
+        })}
+      </div>
+    </motion.div>
+  );
+}
