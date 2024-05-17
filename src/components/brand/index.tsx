698092f0245a0a20b725048b908ac34e8ed70fc3
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-17 11:38:44 +0600

    added animations

--- src/components/brand/index.tsx
+++ src/components/brand/index.tsx
@@ -1,19 +1,44 @@
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion"
 
 export default function Brand() {
   return (
     <section className="w-full aspect-[1923/815] flex justify-center items-center my-8">
-      <div className="relative w-full lg:w-[80%] mx-auto rounded-2xl border border-[#4048c04c] border-b-0 flex flex-col gap-5 pt-10 items-center pb-16 md:pb-24 lg:pb-36">
-        <h1 className="text-center absolute w-full font-bold tracking-wider -bottom-1 lg:-bottom-5 text-5xl md:text-7xl lg:text-9xl uppercase text-muted-foreground overflow-hidden -z-10 opacity-70">
+      <div className="relative w-full lg:w-[80%] mx-auto rounded-2xl border border-[#4048c04c] border-b-0 flex flex-col gap-5 pt-10 items-center pb-16 md:pb-24 lg:pb-36 overflow-hidden">
+        <motion.h1 
+          className="text-center absolute w-full font-bold tracking-wider -bottom-1 lg:-bottom-5 text-5xl md:text-7xl lg:text-9xl uppercase text-muted-foreground -z-10 opacity-70"
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1, transition: { duration: 0.5, type: "tween" } }}
+        >
           LEAPFLOW
-        </h1>
+        </motion.h1>
         <Gradient />
         <div className="flex justify-between gap-5 items-center tracking-wide text-sm text-muted-foreground">
-          <p className="uppercase">Integration</p>
-          <div className="w-2 h-2 bg-secondary rotate-45"></div>
+          <motion.p 
+            className="uppercase"
+            initial={{ x: 25, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1, transition: { type: "tween", duration: 0.5 } }}
+          >
+            Integration
+          </motion.p>
+          <motion.div 
+            className="w-2 h-2 bg-[var(--theme-color)]"
+            initial={{ rotate: 0 }}
+            whileInView={{ rotate: 45 }}
+          />
           <p className="uppercase">Workflows</p>
-          <div className="w-2 h-2 bg-secondary rotate-45"></div>
-          <p className="uppercase">AI Automation</p>
+          <motion.div 
+            className="w-2 h-2 bg-[var(--theme-color)]"
+            initial={{ rotate: 0 }}
+            whileInView={{ rotate: 45, transition: { duration: 1 } }}
+          />
+          <motion.p 
+            className="uppercase"
+            initial={{ x: -25, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1, transition: { type: "tween", duration: 0.5 } }}
+          >
+            AI Automation
+          </motion.p>
         </div>
         <div className="text-lg md:text-3xl text-center">
           <h1>Increase your efficiency with ease,</h1>
@@ -32,7 +57,7 @@ export default function Brand() {
 
 const Gradient = () => {
   return (
-    <svg
+    <motion.svg
       viewBox="0 0 1500 476"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
@@ -40,21 +65,24 @@ const Gradient = () => {
     >
       <g id="gradient">
         <g id="Ellipse 43" filter="url(#filter0_f_129_515)">
-          <ellipse
+          <motion.ellipse
             cx="749.927"
             cy="613.337"
             rx="574.927"
             ry="137.915"
             stroke="white"
-            strokeWidth="120"
+            initial={{ opacity: 0, strokeWidth: 0 }}
+            whileInView={{ opacity: 0.7, strokeWidth: 120 }}
           />
         </g>
         <g id="Ellipse 42 (Stroke)" filter="url(#filter1_f_129_515)">
-          <path
+          <motion.path
             fillRule="evenodd"
             clipRule="evenodd"
             d="M800.5 334.379C458.932 334.379 182.035 390.363 182.035 459.422C182.035 528.481 458.932 584.465 800.5 584.465C1142.07 584.465 1418.96 528.481 1418.96 459.422C1418.96 390.363 1142.07 334.379 800.5 334.379ZM-70 459.422C-70 362.22 319.736 283.422 800.5 283.422C1281.26 283.422 1671 362.22 1671 459.422C1671 556.624 1281.26 635.422 800.5 635.422C319.736 635.422 -70 556.624 -70 459.422Z"
             fill="#5661F6"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, scale: 1.3, transition: { delay: 0.2 } }}
           />
         </g>
       </g>
@@ -102,6 +130,6 @@ const Gradient = () => {
           />
         </filter>
       </defs>
-    </svg>
+    </motion.svg>
   );
 };
