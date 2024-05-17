a867a945656a3ba3801e59ab84d5d7ea50719131
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-17 12:40:07 +0600

    added grid 2

--- src/components/features/grids/grid-2.tsx
+++ src/components/features/grids/grid-2.tsx
@@ -1,7 +1,72 @@
+import { motion } from "framer-motion";
+
 export default function Grid2() {
   return (
-    <div className="col-span-2 border w-full rounded-2xl bg-secondary  flex flex-col justify-center items-center">
-      grid2
+    <div className="col-span-2 border w-full rounded-2xl bg-secondary/40 flex flex-col items-center text-center p-3 lg:p-5">
+      <h1 className="text-xl lg:text-3xl font-bold max-w-lg">
+        Automate your task so that,
+      </h1>
+      <h1 className="text-xl lg:text-3xl font-bold max-w-lg">
+        you can scale growth
+      </h1>
+      <p className="text-muted-foreground text-lg max-w-lg my-5">
+        Scale with ease as you focus on what matters most. Let our intelligent
+        agents handle all the work for you.
+      </p>
+      <div
+        id="graph"
+        className="w-full h-full bg-secondary/30 border rounded-xl p-2 flex flex-col justify-between items-center"
+      >
+        <motion.svg
+          viewBox="0 0 606 199"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-full h-full"
+        >
+          <g id="graph">
+            <motion.path
+              id="Path 887"
+              d="M1.1377 141.432L48.4117 106.576L66.385 117.648L133.686 66.8882L188.01 159.906L236.548 106.576L296.656 117.648L319.251 66.8882L355.156 61.0808L441.156 88.0841L510.69 1L604.69 24.3421"
+              stroke="var(--theme-color)"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              initial={{ pathLength: 0 }}
+              whileInView={{
+                pathLength: 1,
+                strokeWidth: 2,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                transition: { duration: 2 },
+              }}
+            />
+            <motion.path
+              id="Path 888"
+              d="M3.19727 185.976L52.0481 137.459L68.4475 170.496L133.466 133.679L190.078 198L238.618 163.29L298.729 170.496L321.325 137.459L357.231 133.679L452.625 170.496L537.3 84.8066L586.151 137.459"
+              stroke="#4F4F4F"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-dasharray="8 8"
+              initial={{ pathLength: 0 }}
+              whileInView={{
+                pathLength: 1,
+                strokeWidth: 2,
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                transition: { duration: 2 },
+              }}
+            />
+          </g>
+        </motion.svg>
+        <div className="w-full flex justify-between items-center text-xs p-2">
+          <p>01:00PM</p>
+          <p>02:00PM</p>
+          <p>03:00PM</p>
+          <p>04:00PM</p>
+          <p>05:00PM</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
