f3c254e8ff6e9b7c7d8028eefa5b3769f3a1a252
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-17 12:46:43 +0600

    rearranged files

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,20 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
-import App from '@/App'
+import Hero from "@/components/hero";
+import FeaturesSection from "@/components/features";
+import Brand from "@/components/brand";
 
 export const Route = createLazyFileRoute('/')({
   component: App
 })
+
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full">
+      <div className="w-full bg-[var(--bg-hero)] mb-12 lg:mb-[20vh]">
+        <Hero />
+      </div>
+      <FeaturesSection />
+      <Brand />
+    </div>
+  );
+}
