b739591e49dace77f97f6c5178df8e38df55a471
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-17 19:14:04 +0600

    tweaked

--- src/components/ui/gradient-button.tsx
+++ src/components/ui/gradient-button.tsx
@@ -9,6 +9,7 @@ export function HoverBorderGradient({
   children,
   containerClassName,
   className,
+  backgroundClassname,
   as: Tag = "button",
   duration = 1,
   clockwise = true,
@@ -18,6 +19,7 @@ export function HoverBorderGradient({
     as?: React.ElementType;
     containerClassName?: string;
     className?: string;
+    backgroundClassname?: string;
     duration?: number;
     clockwise?: boolean;
   } & React.HTMLAttributes<HTMLElement>
@@ -36,15 +38,15 @@ export function HoverBorderGradient({
 
   const movingMap: Record<Direction, string> = {
     TOP: "radial-gradient(20.7% 50% at 50% 0%, hsl(0, 0%, 100%) 0%, rgba(86, 97, 246, 0) 100%)",
-    LEFT: "radial-gradient(16.6% 43.1% at 0% 50%, hsl(0, 0%, 100%) 0%, rgba(86, 97, 246, 0) 100%)",
+    LEFT: "radial-gradient(40.6% 43.1% at 0% 50%, hsl(0, 0%, 100%) 0%, rgba(86, 97, 246, 0) 100%)",
     BOTTOM:
-      "radial-gradient(20.7% 50% at 50% 100%, hsl(0, 0%, 100%) 0%, rgba(86, 97, 246, 0) 100%)",
+      "radial-gradient(40.7% 50% at 50% 100%, hsl(0, 0%, 100%) 0%, rgba(86, 97, 246, 0) 100%)",
     RIGHT:
-      "radial-gradient(16.2% 41.199999999999996% at 100% 50%, hsl(0, 0%, 100%) 0%, rgba(86, 97, 246, 0) 100%)",
+      "radial-gradient(40.2% 41% at 100% 50%, hsl(0, 0%, 100%) 0%, rgba(86, 97, 246, 0) 100%)",
   };
 
   const highlight =
-    "radial-gradient(75% 181.15942028985506% at 50% 50%, #3275F8 0%, rgba(86, 97, 246, 0) 100%)";
+    "radial-gradient(75% 181% at 50% 50%, #3275F8 0%, rgba(86, 97, 246, 0) 100%)";
 
   useEffect(() => {
     if (!hovered) {
@@ -61,14 +63,14 @@ export function HoverBorderGradient({
       }}
       onMouseLeave={() => setHovered(false)}
       className={cn(
-        "relative flex rounded-full border  content-center bg-black/20 hover:bg-black/10 transition duration-500 dark:bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit",
+        "relative flex rounded-full border content-center bg-black/20 hover:bg-black/10 transition duration-500 dark:bg-white/20 items-center flex-col flex-nowrap gap-10 h-min justify-center overflow-visible p-px decoration-clone w-fit",
         containerClassName
       )}
       {...props}
     >
       <div
         className={cn(
-          "w-auto text-white z-10 bg-[#5661F6]/60 px-4 py-2 rounded-[inherit]",
+          "w-auto z-10 bg-[#5661F6]/60 px-4 py-2 rounded-[inherit]",
           className
         )}
       >
@@ -92,7 +94,7 @@ export function HoverBorderGradient({
         }}
         transition={{ ease: "linear", duration: duration ?? 1 }}
       />
-      <div className="bg-black absolute z-1 flex-none inset-[2px] rounded-[100px]" />
+      <div className={cn("bg-black absolute z-1 flex-none inset-[2px] rounded-[inherit]", backgroundClassname)} />
     </Tag>
   );
 }
