efb79ba5a526bfc036b9b6dde29a2110c3290564
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-17 19:13:47 +0600

    updated for responsiveness

--- src/components/navbar/mininav.tsx
+++ src/components/navbar/mininav.tsx
@@ -50,7 +50,7 @@ const JoinWaitlistButton = () => {
       <span className="absolute inset-0 overflow-hidden rounded-full">
         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(86,97,246,0.6)_0%,rgba(86,97,246,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
       </span>
-      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-background py-0.5 px-4 ring-1 ring-blue-200/10 ">
+      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-background py-0.5 px-1 lg:px-4 ring-1 ring-blue-200/10 ">
         <span>Join waitlist</span>
         <svg
           fill="none"
