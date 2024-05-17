5a90d5a1f47223b1c84f2f6dcfbb57d06babc5f5
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-17 19:44:27 +0600

    init personprofile component

--- src/components/person-profile.tsx
+++ src/components/person-profile.tsx
@@ -3,15 +3,17 @@ import type { PersonProfile as PersonProfileProps } from "@/types"
 
 export default function PersonProfile({ name, designation, image, twitter, linkedin }: PersonProfileProps) {
   return (
-    <div className="max-w-32">
-      <img src={image} className="w-full aspect-square" alt={name} />
-      <h2 className="font-semibold text-lg">{name}</h2>
+    <div className="max-w-[12rem]">
+      <div className="w-full aspect-square rounded-lg overflow-hidden group">
+        <img src={image} className="w-full group-hover:scale-110 duration-200 transition-all transform-gpu ease-in-out" alt={name} />
+      </div>
+      <h2 className="font-semibold text-lg mt-2">{name}</h2>
       <h3 className="text-muted-foreground">{designation}</h3>
-      <div className="flex items-center gap-2">
-        <a href={twitter} target="_blank">
+      <div className="flex items-center gap-5 mt-1">
+        <a href={twitter} target="_blank" className="hover:scale-110 duration-100 ease-in-out transition-all transform-gpu">
           <Icons.Twitter />
         </a>
-        <a href={linkedin} target="_blank">
+        <a href={linkedin} target="_blank" className="hover:scale-110 duration-100 ease-in-out transition-all transform-gpu">
           <Icons.LinkedIn />
         </a>
       </div>
