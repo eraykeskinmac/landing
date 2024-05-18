b641d171a0e710e5ffdeeca8f797a66eaeb9f1a5
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-18 19:00:27 +0600

    new blog

---
+++ src/constants/blogs/announce-waitlist.tsx
@@ -0,0 +1,20 @@
+import type { BlogSchema } from "@/types";
+
+export const announceWaitlist: BlogSchema = {
+  title: "Announcing our waitlist",
+  author: {
+    name: "Suhas Sumukh",
+    designation: "Founder & CEO",
+    image: "/team/suhas.png",
+    twitter: "https://x.com/suhasasumukh",
+    linkedin: "https://www.linkedin.com/in/suhasasumukh"
+  },
+  slug: "announcing-our-waitlist",
+  category: {
+    name: "Announcement",
+    slug: "/blog/announcement"
+  },
+  date: new Date("2024-05-19"),
+  image: "/blog/announcing-our-waitlist.png",
+  content: "We are excited to announce that we are opening our waitlist for our upcoming product. We are building a platform that will help you to manage your tasks and projects in a more efficient way. We are looking for early adopters who are interested in trying out our product and providing us with feedback. If you are interested in joining our waitlist, please sign up below. We will be sending out invites to the waitlist in the coming weeks. We are looking forward to hearing from you!"
+}
