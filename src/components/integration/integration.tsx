2fa8b9904e3cd54021b328639ee9d9c0e9a1685e
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-19 01:08:02 +0600

    initialized integration page

---
+++ src/components/integration/integration.tsx
@@ -0,0 +1,111 @@
+import { cn } from "@/lib/utils";
+import { AnimatedBeam } from "@/components/animated-beam";
+import { Icons } from "@/components/icons";
+import React, { forwardRef, useRef } from "react";
+
+const Circle = forwardRef<
+  HTMLDivElement,
+  { className?: string; children?: React.ReactNode }
+>(({ className, children }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        "z-10 flex h-12 w-12 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+});
+
+export function IntegrationDemo() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const div1Ref = useRef<HTMLDivElement>(null);
+  const div2Ref = useRef<HTMLDivElement>(null);
+  const div3Ref = useRef<HTMLDivElement>(null);
+  const div4Ref = useRef<HTMLDivElement>(null);
+  const div5Ref = useRef<HTMLDivElement>(null);
+  const div6Ref = useRef<HTMLDivElement>(null);
+  const div7Ref = useRef<HTMLDivElement>(null);
+
+  return (
+    <div
+      className="relative flex w-full max-w-[500px] items-center justify-center overflow-hidden rounded-lg p-10"
+      ref={containerRef}
+    >
+      <div className="flex h-full w-full flex-col items-stretch justify-between gap-10">
+        <div className="flex flex-row items-center justify-between">
+          <Circle ref={div1Ref}>
+            <Icons.Discord className="h-6 w-6" />
+          </Circle>
+          <Circle ref={div5Ref}>
+            <Icons.Trello className="h-6 w-6" />
+          </Circle>
+        </div>
+        <div className="flex flex-row items-center justify-between">
+          <Circle ref={div2Ref}>
+            <Icons.notion className="h-6 w-6" />
+          </Circle>
+          <Circle ref={div4Ref} className="h-16 w-16">
+            <Icons.openai className="h-6 w-6" />
+          </Circle>
+          <Circle ref={div6Ref}>
+            <Icons.Linear className="h-6 w-6" />
+          </Circle>
+        </div>
+        <div className="flex flex-row items-center justify-between">
+          <Circle ref={div3Ref}>
+            <Icons.Slack className="h-6 w-6" />
+          </Circle>
+          <Circle ref={div7Ref}>
+            <Icons.LinkedIn className="h-6 w-6" />
+          </Circle>
+        </div>
+      </div>
+
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div1Ref}
+        toRef={div4Ref}
+        curvature={-75}
+        endYOffset={-10}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div2Ref}
+        toRef={div4Ref}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div3Ref}
+        toRef={div4Ref}
+        curvature={75}
+        endYOffset={10}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div5Ref}
+        toRef={div4Ref}
+        curvature={-75}
+        reverse
+        endYOffset={-10}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div6Ref}
+        toRef={div4Ref}
+        reverse
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div7Ref}
+        toRef={div4Ref}
+        curvature={75}
+        reverse
+        endYOffset={10}
+      />
+    </div>
+  );
+}
--- src/routes/integration.lazy.tsx
+++ src/routes/integration.lazy.tsx
@@ -1,5 +1,18 @@
+import { IntegrationDemo } from '@/components/integration/integration'
 import { createLazyFileRoute } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/integration')({
-  component: () => <div>Hello /integration!</div>
+  component: IntegrationPage
 })
+
+function IntegrationPage() {
+  return (
+    <div className='w-full min-h-screen bg-[var(--bg-hero)]'>
+      <h1 className='head-text text-center primary-gradient p-2'>Integration</h1>
+
+      <div className='w-full flex justify-center items-center'>
+        <IntegrationDemo />
+      </div>
+    </div>
+  )
+}
