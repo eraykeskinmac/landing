004fb47f85f0347d959dd56e05df0fda2d252d9f
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-22 20:36:10 +0600

    init integration grid 5

---
+++ src/components/integration/grids/grid-5.tsx
@@ -0,0 +1,11 @@
+export default function Grid5() {
+  return (
+    <div className="w-full h-full flex flex-col justify-end">
+      <div>
+        <h1 className="text-muted-foreground mb-2 primary-gradient">Private teams</h1>
+        <p className="text-lg md:text-xl font-semibold">Control who has access to</p>
+        <p className="text-lg md:text-xl font-semibold">sensitive requests</p>
+      </div>
+    </div>
+  )
+}
