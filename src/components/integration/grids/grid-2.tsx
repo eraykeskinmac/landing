1bbde8f6a1b771e2cceb96a43303fbc735c40981
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-22 23:52:12 +0600

    init grid 2

--- src/components/integration/grids/grid-2.tsx
+++ src/components/integration/grids/grid-2.tsx
@@ -7,22 +7,22 @@ export default function Grid2() {
         <h1 className="primary-gradient text-sm">Work process</h1>
         <p>Your workflows</p>
       </div>
-      <div className="w-full h-full flex flex-col justify-evenly items-center gap-3 p-3">
-        <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center">
+      <div className="w-full h-full flex flex-col justify-evenly items-center gap-3 p-3 font-manrope">
+        <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center cursor-pointer hover:scale-105 duration-200 transition-transform transform-gpu ease-in-out">
           <div className="flex gap-2 items-center">
             <Globe2 size={35} className="bg-secondary p-2 rounded" />
             <h1>Standard Workflow</h1>
           </div>
           <p className="text-muted-foreground text-sm">Run once upon prompt</p>
         </div>
-        <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center">
+        <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center cursor-pointer hover:scale-105 duration-200 transition-transform transform-gpu ease-in-out">
           <div className="flex gap-2 items-center">
             <Globe2 size={35} className="bg-secondary p-2 rounded" />
             <h1>Scheduled Workflow</h1>
           </div>
           <p className="text-muted-foreground text-sm">Runs at a specific time</p>
         </div>
-        <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center">
+        <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center cursor-pointer hover:scale-105 duration-200 transition-transform transform-gpu ease-in-out">
           <div className="flex gap-2 items-center">
             <Globe2 size={35} className="bg-secondary p-2 rounded" />
             <h1>Trigger Workflow</h1>
