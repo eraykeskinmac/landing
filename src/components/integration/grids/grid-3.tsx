22ebbc979084f0a66da91fce06efd029248639c1
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-22 23:52:20 +0600

    init grid 3

--- src/components/integration/grids/grid-3.tsx
+++ src/components/integration/grids/grid-3.tsx
@@ -3,9 +3,18 @@ export default function Grid3() {
     <div className="w-full h-full flex items-center rounded-[inherit]">
       <div className="w-[40%] p-3">
         <h1>Trigger Workflow</h1>
-        <p className="text-lg md:text-xl primary-gradient">Workflows that run at a specific event.</p>
+        <p className="text-[#757EEE] font-semibold text-xl">
+          Workflows that run at a specific event
+        </p>
       </div>
-      <div className="w-[60%] h-full bg-secondary rounded-[inherit]"></div>
+      <DashboardSidebar />
     </div>
   );
 }
+
+const DashboardSidebar = () => {
+  return (
+    <div className="relative w-[60%] h-full bg-secondary rounded-[inherit] translate-x-6 translate-y-6 z-20"> 
+    </div>
+  );
+};
