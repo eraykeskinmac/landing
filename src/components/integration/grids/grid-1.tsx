78324a006b24b0e36d31b2766795545ae0a3bf6a
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-22 20:35:40 +0600

    integration grid 1 init

---
+++ src/components/integration/grids/grid-1.tsx
@@ -0,0 +1,11 @@
+export default function Grid1() {
+  return (
+    <div className="w-full h-full flex flex-col justify-end">
+      <div>
+        <h1 className="text-muted-foreground mb-2">Security</h1>
+        <p className="text-lg md:text-xl font-semibold">Automatically assign or</p>
+        <p className="text-lg md:text-xl font-semibold">notify team member</p>
+      </div>
+    </div>
+  )
+}
