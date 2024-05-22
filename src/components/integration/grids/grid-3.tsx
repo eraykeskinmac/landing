dc88eae576a520200be94325103a94700371ac1e
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-22 20:35:55 +0600

    init integration grid 3

---
+++ src/components/integration/grids/grid-3.tsx
@@ -0,0 +1,11 @@
+export default function Grid3() {
+  return (
+    <div className="w-full h-full flex items-center rounded-[inherit]">
+      <div className="w-[40%] p-3">
+        <h1>Trigger Workflow</h1>
+        <p className="text-lg md:text-xl primary-gradient">Workflows that run at a specific event.</p>
+      </div>
+      <div className="w-[60%] h-full bg-secondary rounded-[inherit]"></div>
+    </div>
+  );
+}
