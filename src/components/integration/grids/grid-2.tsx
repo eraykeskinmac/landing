a35f4e7dd4d2d115a0bd189f41b256a1be4ebc29
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-23 07:49:09 +0600

    replaced globe icons

--- src/components/integration/grids/grid-2.tsx
+++ src/components/integration/grids/grid-2.tsx
@@ -1,30 +1,30 @@
-import { Globe2 } from "lucide-react";
+import { Icons } from "@/components/icons";
 
 export default function Grid2() {
   return (
     <div className="w-full h-full flex flex-col rounded-[inherit] overflow-hidden">
       <div className="bg-background/80 text-center p-2 -z-10">
-        <h1 className="primary-gradient text-sm">Work process</h1>
+        <h1 className="text-[#757EEE] text-sm">Work process</h1>
         <p>Your workflows</p>
       </div>
       <div className="w-full h-full flex flex-col justify-evenly items-center gap-3 p-3 font-manrope">
         <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center cursor-pointer hover:scale-105 duration-200 transition-transform transform-gpu ease-in-out">
           <div className="flex gap-2 items-center">
-            <Globe2 size={35} className="bg-secondary p-2 rounded" />
+            <Icons.Globe />
             <h1>Standard Workflow</h1>
           </div>
           <p className="text-muted-foreground text-sm">Run once upon prompt</p>
         </div>
         <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center cursor-pointer hover:scale-105 duration-200 transition-transform transform-gpu ease-in-out">
           <div className="flex gap-2 items-center">
-            <Globe2 size={35} className="bg-secondary p-2 rounded" />
+            <Icons.Globe />
             <h1>Scheduled Workflow</h1>
           </div>
           <p className="text-muted-foreground text-sm">Runs at a specific time</p>
         </div>
         <div className="w-full bg-background/40 rounded-lg p-3 flex flex-col justify-center cursor-pointer hover:scale-105 duration-200 transition-transform transform-gpu ease-in-out">
           <div className="flex gap-2 items-center">
-            <Globe2 size={35} className="bg-secondary p-2 rounded" />
+            <Icons.Globe />
             <h1>Trigger Workflow</h1>
           </div>
           <p className="text-muted-foreground text-sm">Run when a specific event occurs</p>
