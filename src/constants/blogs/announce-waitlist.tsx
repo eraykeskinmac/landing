9467acc3105b5063435a33e5d95667c6f1159bb9
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-23 13:24:36 +0600

    updated blog date

--- src/constants/blogs/announce-waitlist.tsx
+++ src/constants/blogs/announce-waitlist.tsx
@@ -15,7 +15,7 @@ export const announceWaitlist: BlogSchema = {
     name: "Announcement",
     slug: "/blog/announcement"
   },
-  date: new Date("2024-05-19"),
+  date: new Date("2024-05-23"),
   image: "/blog/announcing-our-waitlist.png",
   content: "We are excited to announce that we are opening our waitlist for our upcoming product. We are building a platform that will help you to manage your tasks and projects in a more efficient way. We are looking for early adopters who are interested in trying out our product and providing us with feedback. If you are interested in joining our waitlist, please sign up below. We will be sending out invites to the waitlist in the coming weeks. We are looking forward to hearing from you!"
 }
