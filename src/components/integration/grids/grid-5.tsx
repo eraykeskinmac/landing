5d4ad2664e91049668323fa233cf2115af6e8ee7
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-23 17:22:05 +0600

    added animations

--- src/components/integration/grids/grid-5.tsx
+++ src/components/integration/grids/grid-5.tsx
@@ -1,88 +1,155 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { motion } from "framer-motion";
+import { AnimatedBeam } from "@/components/animated-beam";
+import { cn } from "@/lib/utils";
+import { GridInput } from "./grid-input";
 
 export default function Grid5() {
   const [value, setValue] = useState("");
   const [animating, setAnimating] = useState(false);
 
+  // refs
+  const containerRef = useRef(null);
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const fromRef = useRef(null);
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     !animating && animateBackground();
-  }
-  
+  };
+
   const animateBackground = () => {
-    setAnimating(true);
-  }
+    !animating && setAnimating(true);
+
+    const timeoutId = setTimeout(() => {
+      setAnimating(false);
+      console.log("Task added: ", value);
+    }, 5000);
+
+    return () => clearTimeout(timeoutId);
+  };
 
   return (
-    <div className="w-full h-full flex flex-col justify-end gap-12">
-      <motion.div
-        className="w-full h-full border rounded-2xl bg-[var(--bg-hero)] flex flex-col justify-between p-4 gap-5"
-        // initial={{ backgroundColor: "#110D22"}}
-        // animate={{ backgroundColor: ["#110D22", "var(--theme-color)"] }}
-      >
+    <div
+      ref={containerRef}
+      className="w-full h-full flex flex-col justify-end gap-12"
+    >
+      <motion.div className="w-full h-full border rounded-2xl bg-secondary flex flex-col justify-between p-4 gap-5">
         <div className="space-y-3">
           <p>Updating task status</p>
           <div className="flex gap-2 text-xs flex-wrap">
-            <p className="py-1 px-2 bg-indigo-500 rounded-full cursor-pointer hover:bg-indigo-500/60 duration-150 transition-all ease-in-out">
+            <p
+              ref={ref1}
+              className={cn(
+                "py-1 px-2 bg-black/40 rounded-full cursor-pointer hover:bg-black/60 duration-150 transition-all ease-in-out z-20",
+                animating && " shadow-lg shadow-indigo-500/20 delay-300"
+              )}
+              onClick={animateBackground}
+            >
               Notify team members
             </p>
-            <p className="py-1 px-2 bg-indigo-500 rounded-full cursor-pointer hover:bg-indigo-500/60 duration-150 transition-all ease-in-out">
+            <p
+              ref={ref2}
+              className={cn(
+                "py-1 px-2 bg-black/40 rounded-full cursor-pointer hover:bg-black/60 duration-150 transition-all ease-in-out z-20",
+                animating && " shadow-lg shadow-indigo-500/20 delay-300"
+              )}
+              onClick={animateBackground}
+            >
               Add a note to this log
             </p>
-            <p className="py-1 px-2 bg-indigo-500 rounded-full cursor-pointer hover:bg-indigo-500/60 duration-150 transition-all ease-in-out">
+            <p
+              ref={ref3}
+              className={cn(
+                "py-1 px-2 bg-black/40 rounded-full cursor-pointer hover:bg-black/60 duration-150 transition-all ease-in-out z-20",
+                animating && " shadow-lg shadow-indigo-500/20 delay-300"
+              )}
+              onClick={animateBackground}
+            >
               Create a meeting
             </p>
           </div>
         </div>
-        <div className="w-full h-full max-h-12 bg-indigo-900/10 border border-indigo-900/50 rounded-full flex justify-between items-center text-muted-foreground">
-          <div className="px-4 flex items-center gap-2">
-            <span>🧠</span>
-            <form onSubmit={handleSubmit}>
-              <input
-                className="border-none outline-none bg-transparent text-sm"
-                placeholder="Give a task..."
-                onChange={(e) => setValue(e.target.value)}
-              />
-            </form>
-          </div>
-          <button
-            disabled={!value}
-            type="submit"
-            className="h-8 w-8 mr-1 rounded-full bg-[var(--theme-color)] transition duration-200 flex items-center justify-center"
-          >
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7.39969 6.32015L15.8897 3.49015C19.6997 2.22015 21.7697 4.30015 20.5097 8.11015L17.6797 16.6002C15.7797 22.3102 12.6597 22.3102 10.7597 16.6002L9.91969 14.0802L7.39969 13.2402C1.68969 11.3402 1.68969 8.23016 7.39969 6.32015Z"
-                stroke="#c7c7c7"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M10.1094 13.6498L13.6894 10.0598"
-                stroke="#c7c7c7"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </button>
-        </div>
+        <GridInput
+          placeholders={[
+            "Give a task...",
+            "Notify team members",
+            "Add a note to this log",
+            "Create a meeting",
+          ]}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          onSubmit={(e) => handleSubmit(e)}
+          id="grid-form"
+          ref={fromRef}
+        />
       </motion.div>
       <div>
-        <h1 className="mb-2 text-[#757EEE]">Intelligent Task Assistance</h1>
+        <h1 className="mb-2 text-[#757EEE] font-medium">
+          Intelligent Task Assistance
+        </h1>
         <p className="text-lg md:text-xl font-semibold">
           Get smart suggestions for your workflow from
         </p>
         <p className="text-lg md:text-xl font-semibold">our action model.</p>
       </div>
+      <AnimatedBeam
+        containerRef={containerRef}
+        toRef={ref1}
+        fromRef={fromRef}
+        curvature={0}
+        startXOffset={17}
+        startYOffset={17}
+        endXOffset={20}
+        endYOffset={30}
+        reverse
+        gradientStartColor="var(--indigo-500)"
+        gradientStopColor="var(--indigo-800)"
+        pathOpacity={animating ? 0.5 : 0}
+        className={cn(
+          "opacity-0 transition-all duration-500 ease-in-out",
+          animating && "opacity-100"
+        )}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        toRef={ref2}
+        fromRef={fromRef}
+        curvature={0}
+        startXOffset={17}
+        startYOffset={17}
+        endXOffset={20}
+        endYOffset={30}
+        reverse
+        gradientStartColor="var(--indigo-500)"
+        gradientStopColor="var(--indigo-800)"
+        pathOpacity={animating ? 0.5 : 0}
+        className={cn(
+          "opacity-0 transition-all duration-500 ease-in-out",
+          animating && "opacity-100"
+        )}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        toRef={ref3}
+        fromRef={fromRef}
+        curvature={0}
+        endXOffset={20}
+        startXOffset={17}
+        startYOffset={17}
+        endYOffset={30}
+        reverse
+        gradientStartColor="var(--indigo-500)"
+        gradientStopColor="var(--indigo-800)"
+        pathOpacity={animating ? 0.5 : 0}
+        className={cn(
+          "opacity-0 transition-all duration-500 ease-in-out",
+          animating && "opacity-100"
+        )}
+      />
     </div>
   );
 }
