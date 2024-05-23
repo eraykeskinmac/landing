167fc694221d2c8f8907e98a29593ffef6b7e974
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-23 09:39:10 +0600

    added interactiveness

--- src/components/integration/grids/grid-3.tsx
+++ src/components/integration/grids/grid-3.tsx
@@ -2,10 +2,10 @@ import { Icons } from "@/components/icons";
 
 export default function Grid3() {
   return (
-    <div className="w-full h-full flex p-3 items-center rounded-[inherit]">
+    <div className="w-full h-full flex p-3 items-center rounded-[inherit] group/grid3">
       <div className="w-[40%]">
         <h1>Trigger Workflow</h1>
-        <p className="text-[#757EEE] font-semibold text-xl">
+        <p className="text-[#757EEE] font-semibold text-xl group-hover/grid3:scale-105 group-hover/grid3:translate-x-1 duration-300 delay-75 transition-all ease-in-out">
           Workflows that run at a specific event
         </p>
       </div>
@@ -16,14 +16,14 @@ export default function Grid3() {
 
 const DashboardSidebar = () => {
   return (
-    <div className="relative w-[60%] bg-secondary rounded-[inherit] space-y-3 p-3">
-      <div className="bg-black/40 rounded-[inherit] p-2 md:p-3 text-muted-foreground text-sm">
+    <div className="relative w-[60%] bg-secondary rounded-[inherit] space-y-3 p-3 group-hover/grid3:bg-secondary/80 duration-300 delay-75 transition-all transform-gpu ease-in-out">
+      <div className="bg-black/40 rounded-[inherit] p-2 md:p-3 text-muted-foreground text-sm group-hover/grid3:scale-105 group-hover/grid3:-translate-x-6 duration-300 delay-75 transition-all transform-gpu ease-in-out">
         <p>
           When I receive a new message in the Slack #issue channel, create a new
           ticket in my Jira general issue project.
         </p>
       </div>
-      <div className="bg-black/40 rounded-[inherit] p-2 md:p-3">
+      <div className="bg-black/40 rounded-[inherit] p-2 md:p-3 border border-transparent group-hover/grid3:border-[var(--theme-color)] group-hover/grid3:shadow-2xl duration-300 delay-75 transition-all transform-gpu ease-in-out">
         <div className="flex items-center gap-2">
           <Icons.Globe />
           <h1 className="text-sm">Create a ticket trigger workflow</h1>
