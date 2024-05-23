ba8cf196e01a3443ee1ac24ff638c5e33b5a2b90
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-23 17:54:31 +0600

    refactored link

--- src/components/hero/announcement-bar.tsx
+++ src/components/hero/announcement-bar.tsx
@@ -3,7 +3,7 @@ import { ArrowRight } from "lucide-react";
 export default function AnnouncementBar() {
   return (
     <a
-      href="/blog/announcing-our-waitlist"
+      href="./blog/announcing-our-waitlist"
       className="nav-gradient border-indigo-800/80 border border-b-0 rounded-full flex justify-between p-1 text-xs items-center gap-2 shine shine-hover hover:scale-105 duration-100 transition-transform transform-gpu ease-in-out cursor-pointer group"
     >
       <div className="px-[0.35rem] py-[0.125rem] bg-[var(--theme-color)] rounded-xl">
