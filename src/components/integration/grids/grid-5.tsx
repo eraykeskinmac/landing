36e8299ab47aa3a7c66fe7fbb87944d545adfbf6
rudrodip <official.rudrodipsarker@gmail.com>
2024-05-23 07:16:52 +0600

    updated text color

--- src/components/integration/grids/grid-5.tsx
+++ src/components/integration/grids/grid-5.tsx
@@ -2,7 +2,7 @@ export default function Grid5() {
   return (
     <div className="w-full h-full flex flex-col justify-end">
       <div>
-        <h1 className="text-muted-foreground mb-2 primary-gradient">Private teams</h1>
+        <h1 className="mb-2 text-[#757EEE]">Private teams</h1>
         <p className="text-lg md:text-xl font-semibold">Control who has access to</p>
         <p className="text-lg md:text-xl font-semibold">sensitive requests</p>
       </div>
